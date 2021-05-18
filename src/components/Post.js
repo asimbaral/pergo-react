b3889cdc992b5424af5dbf67903ad0fdd25b3edf
Asim <baralasim6@gmail.com>
2021-05-17 23:10:49 -0400

    Added like-button binding to function that updates it

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 const Post = ({post}) => {
-    const likeAction = (e) => {
-        e.preventDefault();
+    const [likeCount, setLikeCount] = useState(post.likes);
+    const [likeUpdateCount, setLikeUpdateCount] = useState(1);
+    const apiURL = "http://localhost:8080/api/posts/";
+    const likeAction = () => {
+      console.log("likeUpdateCount: ", likeUpdateCount, " post.likes: ", post.likes);
+        const likeUpdate = {
+          "likes": (post.likes + likeUpdateCount)
+        };
+        fetch((apiURL + post._id), {
+          method: 'PUT',
+          body: JSON.stringify(likeUpdate),
+          headers: {
+              'content-type': 'application/json'
+          }
+        }).then(e => {
+          console.log(likeUpdateCount);
+          setLikeCount(post.likes + likeUpdateCount);
+          post.likes = post.likes + likeUpdateCount;
+          const upc = (likeUpdateCount === 1) ? -1 : 1;
+          setLikeUpdateCount(upc);
+          console.log(likeUpdateCount);
+        });
     }
 
-    const postData = (new Date(post.createdAt));
-    
+    const postDate = (new Date(post.createdAt));
 
     return (
         <div className="post">
             <footer className="postHeader">
                 <h4>{post.type}</h4>
-                <h2>{postData.getMonth() + "-" + postData.getDate() + "-" + postData.getFullYear()}</h2>
+                <h2>{postDate.getMonth() + "-" + postDate.getDate() + "-" + postDate.getFullYear()}</h2>
             </footer>
           <p>{post.content}</p>
           <footer className="postBottom">
-            <i class="far fa-heart"></i>
+            <i class="far fa-heart" onClick={likeAction}></i>
             <i class="far fa-comments"></i>
             <i class="far fa-share-square"></i>
           </footer>
           <footer className="postStat">
-            <h5>{post.likes}</h5>
-            <h5>{post.likes}</h5>
-            <h5>{post.likes}</h5>
+            <h5>{likeCount}</h5>
+            <h5>0</h5>
+            <h5>0</h5>
           </footer>
         </div>
     

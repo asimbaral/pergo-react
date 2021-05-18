7c21bc6883db3fa668efe4498d09581fed7d62e5
Asim <baralasim6@gmail.com>
2021-05-18 02:20:42 -0400

    Added modal for comments, and likes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,16 @@
 import React, { useState }  from 'react';
+import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 const Post = ({post}) => {
     const [likeCount, setLikeCount] = useState(post.likes);
     const [likeUpdateCount, setLikeUpdateCount] = useState(1);
+    const [commentCount, setCommentCount] = useState(post.comments.length);
+    const [shareCount, setShareCount] = useState(post.shares);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     const apiURL = "http://localhost:8080/api/posts/";
     const likeAction = () => {
-      console.log("likeUpdateCount: ", likeUpdateCount, " post.likes: ", post.likes);
         const likeUpdate = {
           "likes": (post.likes + likeUpdateCount)
         };
@@ -16,34 +21,46 @@ const Post = ({post}) => {
               'content-type': 'application/json'
           }
         }).then(e => {
-          console.log(likeUpdateCount);
           setLikeCount(post.likes + likeUpdateCount);
           post.likes = post.likes + likeUpdateCount;
           const upc = (likeUpdateCount === 1) ? -1 : 1;
           setLikeUpdateCount(upc);
-          console.log(likeUpdateCount);
         });
+    }
+
+    function toggleModal() {
+      setModalIsOpen(!modalIsOpen);
     }
 
     const postDate = (new Date(post.createdAt));
 
     return (
         <div className="post">
-            <footer className="postHeader">
-                <h4>{post.type}</h4>
-                <h2>{postDate.getMonth() + "-" + postDate.getDate() + "-" + postDate.getFullYear()}</h2>
-            </footer>
+          <footer className="postHeader">
+              <h4>{post.type}</h4>
+              <h2>{postDate.getMonth() + "-" + postDate.getDate() + "-" + postDate.getFullYear()}</h2>
+          </footer>
           <p>{post.content}</p>
           <footer className="postBottom">
             <i class="far fa-heart" onClick={likeAction}></i>
-            <i class="far fa-comments"></i>
+            <i class="far fa-comments" onClick={toggleModal}></i>
             <i class="far fa-share-square"></i>
           </footer>
           <footer className="postStat">
             <h5>{likeCount}</h5>
-            <h5>0</h5>
-            <h5>0</h5>
+            <h5>{commentCount}</h5>
+            <h5>{shareCount}</h5>
           </footer>
+          <Modal isOpen={modalIsOpen} onRequestClose={toggleModal} contentLabel={post.name + "'s post"}>
+            <h6>{post.name + "'s post"}</h6>
+            <p>{post.content}</p>
+            <form>
+              <textarea>Enter comment here</textarea>
+              <button onClick={toggleModal}>Submit</button>
+            </form>
+            
+            <button onClick={toggleModal}>Close</button>
+          </Modal>
         </div>
     
     );
